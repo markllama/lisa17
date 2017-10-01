1dc0f3030da5e55cb01c4d615d028adfe5fd512d
Mark Lamourine <markllama@gmail.com>
2017-09-30 21:54:46 -0400

    refactored provisioning out

---
+++ atomic-vagrant/provision_etcd.rb
@@ -0,0 +1,52 @@
+#
+# Utility Functions for privisioning container hosts
+#
+
+def block_start(instance_num)
+  instance_num < $num_instances ? " --no-block" : ""
+end
+
+def forward_cockpit(vm, instance_num)
+  # export the ports for cockpit on each instance
+  vm.network :forwarded_port, guest: 9090, host: 9090 + instance_num
+end
+
+def enable_eth1(vm)
+  # bring up the private/container network
+  vm.provision :shell, :name => "enable eth1",
+              :inline => "ifup eth1",
+              :privileged => true
+end
+
+
+def configure_etcd(vm, instance_num)
+# resolve the etcd configuration from the template and host list
+  vm.provision :shell, :name => "gen config",
+               :inline => "python3 sync/transform.py sync/etcd.conf.j2 %s < sync/hosts.yml > etcd.conf" % vm.hostname
+
+  # place the etcd configuration
+  vm.provision :shell, :name => "set config",
+               :inline => "cp etcd.conf /etc/etcd",
+               :privileged => true
+
+  vm.provision :shell,
+               :name => "start etcd" + block_start(instance_num),
+               :inline => "systemctl start etcd" + block_start(instance_num),
+               :privileged => true
+end
+
+def configure_flanneld(vm, instance_num)
+  vm.provision :shell, :name => "set flannel iface",
+               :inline => "sed -i '$aFLANNEL_OPTIONS=\"--iface=eth1\"' /etc/sysconfig/flanneld",
+               :privileged => true
+      
+  # Only set the flannel configuration in etcd once
+  if instance_num == $num_instances then
+    vm.provision :shell, :name => "set flannel cfg",
+                 :inline => "etcdctl set /atomic.io/network/config \'{ \"Network\": \"172.24.0.0/16\", \"Backend\": {\"Type\": \"vxlan\"}}\'"
+  end
+
+  vm.provision :shell, :name => "flannel service" + block_start(instance_num),
+               :inline => "systemctl start flanneld" + block_start(instance_num),
+               :privileged => true
+end
